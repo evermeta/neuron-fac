e91572ad5e761e8a3595bcc90feb1f5388374dd1
FranckEinstein90 <franckbinard@gmail.com>
2022-08-07 04:39:57 +0100

    cleanup: removed dist files from repo

--- dist/tests/interactive/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const types_1 = require("../../src/utils/console/interactive/types");
-const interactiveConsole = new types_1.ConsoleInteractive();
-const p = interactiveConsole.promptUser().then((_) => "Interactive Test Is Over");
