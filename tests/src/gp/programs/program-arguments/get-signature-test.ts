6e49e5ba1cc0071faf645d351a12af8c77e0643a
FranckEinstein90 <franckbinard@gmail.com>
2022-08-07 04:42:29 +0100

    coverage: get-signature-test

--- tests/src/gp/programs/program-arguments/get-signature-test.ts
+++ tests/src/gp/programs/program-arguments/get-signature-test.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import { getSignature } from "../../../../../src/gp/programs/program-arguments/get-signature";
+import { Program } from "../../../../../src/gp/programs/program-class";
 
 describe("Program typeSignature property", () => {
     it("It returns the type signature of the program", () => {
@@ -11,14 +12,18 @@ describe("Program typeSignature property", () => {
         expect(testSignature).to.deep.equal("number => Whatever");
     });
 
-/*    it("It returns the type signature of the program", () => {
+    it("It returns the type signature of the program", () => {
         const progArgs = {
             d: { type: "string", index: 1 },
             b: { type: "number", index: 0 },
         };
-        const program = new Program("jsOneLiner", progArgs, "b*3");
+        const program = new Program(
+            "jsOneLiner", 
+            progArgs, 
+            'number', 
+            "b*3");
         expect(program.typeSignature).to.deep.equal(
-            "number => string => ProgramOutputType"
+            "number => string => number"
         );
-    });*/
+    });
 });
