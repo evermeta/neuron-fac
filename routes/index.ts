cdadd5bd462fd07ef799e0ffb556d17120f28151
franckEinstein90 <franckbinard@gmail.com>
2023-01-21 22:58:51 -0500

    changes???

--- routes/index.ts
+++ routes/index.ts
@@ -4,15 +4,20 @@ as the route
 *******************************************************************************/
 import fs from "fs";
 import express, {Request, Response} from "express";
-import { IApplication, NeuronFacApp, PageHandler } from "../src/server/types";
+import { IApplication, PageHandler } from "../src/server/types";
 import rateLimit from "express-rate-limit";
+import { NeuronFacApp } from "../src/server/neuronFacApplication";
 
 /******************************************************************************/
 
 export const newIndexRouter = (
     route = "", 
     app: NeuronFacApp, 
-    subApp: IApplication | null): PageHandler => {
+    options: {
+        subApp: IApplication | null, 
+        renderView:boolean | null
+        },
+    ): PageHandler => {
 
     const expressRouter = express.Router( );
     const viewPath = route === "" ? "index" : route ;
@@ -22,16 +27,21 @@ export const newIndexRouter = (
         windowMs: 15 * 60 * 1000, // 15 minutes
         max: 100, // limit each IP to 100 requests per windowMs
     })
+    
     expressRouter.use(limiter); 
+
     expressRouter.get(httpRootPath, async (req: Request, res: Response) => {
         const appServerData = await app.appServerData(httpRootPath);
-        res.render(viewPath, appServerData);
+        if(options.renderView){
+            res.render(viewPath, appServerData);
+        }
     });
 
     app.expressApp.use(expressRouter);
-    if(subApp) {
-        app.addSubApp(httpRootPath, subApp);
+    if(options.subApp) {
+        app.addSubApp(httpRootPath, options.subApp);
     }
+
     return {
         expressRouter,
     };
