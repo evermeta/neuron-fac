62bad2c980d016901946637140755b7495370452
franckEinstein90 <franckbinard@gmail.com>
2023-01-30 08:58:08 -0500

    work on dialogflow answer response flow

--- src/utils/dialogflow/dialogflow-index.ts
+++ src/utils/dialogflow/dialogflow-index.ts
@@ -11,8 +11,21 @@ interface IFulfillementResponse {
     messages: IResponseMessage[];
 }
 
-export interface ISessionInfo {
-    parameters: Record<string,  string | number>;
+export type ParametersObjectType = Record<string, string | number>;
+
+const errorAsObject = (() => {
+  let ctr = 0;
+  return {
+    createError: (errorMessage: string) => {
+      ctr++;
+      return {
+        ["error (" + ctr + ")"]: errorMessage
+      };
+    }
+  };}) ();
+
+export interface ISessionInfo{
+   parameters: ParametersObjectType; 
 }
 
 export const addSessionVariable = (
@@ -24,13 +37,35 @@ export const addSessionVariable = (
 
 export interface IDialogflow_webhook_answer {
     fulfillmentResponse?: IFulfillementResponse;
-    sessionInfo?: ISessionInfo;
-};
+    sessionInfo?:  ISessionInfo};
 
-export const strWebhookAnswer = (outputParameters: Record<string, string | number>): string => {
+export const strWebhookAnswer = (outputParameters: ParametersObjectType): string => {
     return JSON.stringify({
        sessionInfo: {
             parameters: outputParameters
         }
     });
 };
+
+export const getRequestParameter =  (
+    sessionInfo: any, 
+    parameterName: string): Record<string, string | number> => {
+
+    if(!sessionInfo) return errorAsObject.createError('sessionInfo not found');
+    if(!sessionInfo.parameters) return errorAsObject.createError('sessionInfo.parameters not found'); 
+    if(!(parameterName in sessionInfo.parameters)) return errorAsObject.createError(`Variable ${parameterName} not found`);
+    return {[parameterName]: sessionInfo.parameters[parameterName]};
+};
+
+
+export const subWebhook = async (
+    inputs: ParametersObjectType, 
+    subApplication: (inputs: ParametersObjectType) => Promise<ParametersObjectType>): Promise<IDialogflow_webhook_answer> => {
+    const outputParameters = await subApplication(inputs);
+    return {
+        sessionInfo: {
+            parameters: outputParameters
+        }
+    };
+}
+
--- tests/src/utils/dialogflow/dialogflow-tests.ts
+++ tests/src/utils/dialogflow/dialogflow-tests.ts
@@ -1,7 +1,7 @@
 /******************************************************************************/
 
 import { expect } from "chai";
-import { addSessionVariable, IDialogflow_webhook_answer, ISessionInfo, strWebhookAnswer } from "../../../../src/utils/dialogflow/dialogflow-index";
+import { addSessionVariable, getRequestParameter, IDialogflow_webhook_answer, ISessionInfo, ParametersObjectType, strWebhookAnswer, subWebhook } from "../../../../src/utils/dialogflow/dialogflow-index";
 /******************************************************************************/
 describe("The ISessionInfo interface", () => {
     it("helper function: addSessionVariable allows adding a session variable to an answer", () => {
@@ -9,18 +9,27 @@ describe("The ISessionInfo interface", () => {
         addSessionVariable(sessionInfo, "testKey", "testValue");
         const testAnswer = JSON.stringify({ sessionInfo });
         const expectedAnswer = "{\"sessionInfo\":{\"parameters\":{\"testKey\":\"testValue\"}}}";
-        expect(JSON.stringify(testAnswer)).to.eq(expectedAnswer);
+        expect(testAnswer).to.eq(expectedAnswer);
     });
 
     it("helper function: strWebhookAnswer", () => {
-        const testAnswer = strWebhookAnswer({
+        it("Returns a string", ()=>{
+            const testAnswer = strWebhookAnswer({
             userId: "X$fdsa9",
             userName: "franck",
-        }) ;
-        const expectedAnswer = "{\"userId\":\"X$fdsa9\",\"userName\":\"franck\"}"; 
-        expect(JSON.stringify(testAnswer)).to.eq(expectedAnswer);
+            }) ;
+            expect(testAnswer).to.be.a.string;
+        });
+        it('The string it returns is a valid sessionInfo object in json', ()=>{
+            const testAnswer = strWebhookAnswer({
+                userId: "X$fdsa9",
+                userName: "franck",
+            }) ;
+            const expectedAnswer = "{\"sessionInfo\":{\"parameters\":{\"userId\":\"X$fdsa9\",\"userName\":\"franck\"}}}";
+            expect(testAnswer).to.eq(expectedAnswer);
+            expect(JSON.parse(testAnswer)).to.eq(JSON.parse(expectedAnswer));
+        })
     });
-
 });
 /******************************************************************************/
 describe("IDialoflow_webhook_answer interface", () => {
@@ -34,4 +43,26 @@ describe("IDialoflow_webhook_answer interface", () => {
         const expectedAnswer = "{\"fulfillmentResponse\":{\"messages\":[{\"text\":{\"text\":[\"test message\"]}}]}}";
         expect(testAnswer).to.eq(expectedAnswer);
     });
+});
+
+describe("getRequestParameter function", ()=> {  
+    const sessionInfo: ISessionInfo = { parameters: {} };
+    addSessionVariable(sessionInfo, "testKey", "testValue");
+    it("returns an object", () => {
+        const result = getRequestParameter(sessionInfo, "testKey");
+        expect(result).to.be.an("object");
+    });
+});
+
+describe("The subwebHook function", async ()=> {  
+
+    it("returns an object", async () => {
+        const appTest = async (inputs: ParametersObjectType): Promise<ParametersObjectType> => {
+            const a: number = inputs['a'] as number;
+            return { a: a + 1 } 
+        }
+        const hookOutput = await subWebhook({ a: 1 }, appTest);
+        expect(hookOutput).to.be.an("object");
+    });
+
 });
